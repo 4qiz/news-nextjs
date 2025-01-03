7b7918c998e99fa17a1e68138d025e02b478aeac
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2025-01-03 23:27:20 +0300

    Hero Section

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Share idea</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Get noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
+}
